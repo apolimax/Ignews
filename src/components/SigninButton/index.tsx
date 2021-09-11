f2fdfe27cf3dab689969d578ad1b5394d3ec43a6
Will Apolinario <willieapolinario@gmail.com>
2021-09-11 10:23:17 -0300

    added oauth authentication with github

---
+++ src/components/SigninButton/index.tsx
@@ -0,0 +1,33 @@
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from "next-auth/client";
+// import { signIn, useSession } from "next-auth/react";
+
+import styles from './styles.module.scss'
+
+export function SigninButton() {
+    const [session] = useSession();
+
+    console.log(session)
+
+    return session ? (
+        <button
+            type="button"
+            className={styles.signinButton}
+            onClick={() => signOut()}
+        >
+            <FaGithub color="#04d361" />
+            {session.user.name}
+            <FiX color="#737380" className={styles.closeIcon} />
+        </button>
+    ) : (
+        <button
+            type="button"
+            className={styles.signinButton}
+            onClick={() => signIn('github')}
+        >
+            <FaGithub color="#eba417" />
+            Sign in with Github
+        </button>
+    )
+}
